26049a0a667bb2a1102db15b10818b11bd60cd1e
louis.knight <louis.knight@edointeractive.com>
2015-03-19 13:45:26 -0500

    Converted models into serializable.

--- src/main/java/Madness/model/Game.java
+++ src/main/java/Madness/model/Game.java
@@ -1,12 +1,10 @@
 package Madness.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.io.Serializable;
 
 @Entity
-public class Game {
+public class Game implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
